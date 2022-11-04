1d9bb0f7a2266a2df6743a9ed9597b636f118ac9
Адильбек Джарасов <dzharasov97@gmail.com>
2022-11-04 13:12:41 +0600

    Добавил ';' в блоках кода второго задания, прописал type=application/javascript в index.html

--- task2.js
+++ task2.js
@@ -1,6 +1,6 @@
 const FirstList = [1, 5, 12, 4, 3];
-const SecondList = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-const ThirdList = [3, 10, 17]
+const SecondList = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+const ThirdList = [3, 10, 17];
 
 function count(array) {
     let sum = 0;
