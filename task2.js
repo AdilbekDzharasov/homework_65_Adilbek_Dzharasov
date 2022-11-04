c40c2ef3dbeca03023dcbe206a66bcd5c5670d30
Адильбек Джарасов <dzharasov97@gmail.com>
2022-11-04 12:46:24 +0600

    Добавлен метод решения второго задания

---
+++ task2.js
@@ -0,0 +1,15 @@
+const FirstList = [1, 5, 12, 4, 3];
+const SecondList = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+const ThirdList = [3, 10, 17]
+
+function count(array) {
+    let sum = 0;
+    array.forEach(function(item){
+        sum += item
+    })
+    return console.log(sum / array.length);
+}
+count(FirstList);
+count(SecondList);
+count(ThirdList);
+
